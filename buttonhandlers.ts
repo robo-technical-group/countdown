f82ea7a0dadc73cab7c86578bb40165b38a1ea88
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2026-09-05 16:23:59 -0400

    Add multiplayer button handlers.

---
+++ buttonhandlers.ts
@@ -0,0 +1,29 @@
+namespace ButtonHandlers {
+    export function A(player: number): void {
+
+    }
+
+    export function B(player: number): void {
+
+    }
+    export function Down(player: number): void {
+
+    }
+
+    export function Left(player: number): void {
+
+    }
+
+    export function Menu(player: number): void {
+
+    }
+
+    export function Right(player: number): void {
+
+    }
+
+    export function Up(player: number): void {
+
+    }
+
+}
---
+++ controllermp.ts
@@ -0,0 +1,88 @@
+/**
+ * Player 1
+ */
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.A(1)
+})
+controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.B(1)
+})
+controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Up(1)
+})
+controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Down(1)
+})
+controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Left(1)
+})
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Right(1)
+})
+controller.menu.onEvent(ControllerButtonEvent.Pressed, () => {})
+
+/**
+ * Player 2
+ */
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.A(2)
+})
+controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.B(2)
+})
+controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Up(2)
+})
+controller.player2.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Down(2)
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Left(2)
+})
+controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Right(2)
+})
+
+/**
+ * Player 3
+ */
+controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.A(3)
+})
+controller.player3.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.B(3)
+})
+controller.player3.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Up(3)
+})
+controller.player3.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Down(3)
+})
+controller.player3.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Left(3)
+})
+controller.player3.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Right(3)
+})
+
+/**
+ * Player 4
+ */
+controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.A(4)
+})
+controller.player4.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.B(4)
+})
+controller.player4.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Up(4)
+})
+controller.player4.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Down(4)
+})
+controller.player4.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Left(4)
+})
+controller.player4.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    ButtonHandlers.Right(4)
+})
