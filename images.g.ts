144506ba7b8b269e63b04c430987c3c57a9211c7
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2026-09-05 15:58:47 -0400

    Add enums and initial assets.

---
+++ images.g.ts
@@ -0,0 +1,356 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "RTGLogo":return img`
+3ccccccccccccccccccccccccccccccccccccccccccccccb
+cbccccccccccccccccccccccccccccccccccccccccccccbf
+ccbccccccccccccccccccccccccccccccccccccccccccbff
+cccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbfffff55555555555555555555555ffffffffffffbfff
+cccbffffff55555555555555555555555fffffffffffbfff
+cccbffffffff5555555555555555555555ffffffffffbfff
+cccbfffffffff5555555555555555555555fffffffffbfff
+cccbfffffffff55555555555555555555555ffffffffbfff
+cccbfffffffff555555555555555555555555fffffffbfff
+cccbfffffffff555555fffffffffff55555555ffffffbfff
+cccbfffffffff555555ffffffffffff55555555fffffbfff
+cccbfffffffff555555fffffffffffff5555555fffffbfff
+cccbfffffffff555555ffffffffffff55555555fffffbfff
+cccbfffffffff555555fffffffffff55555555ffffffbfff
+cccbfffffffff555555555555555555555555fffffffbfff
+cccbfffffffff55555555555555555555555ffffffffbfff
+cccbfffffffff5555555555555555555555fffffffffbfff
+cccbfffffffff555555555555555555555ffffffffffbfff
+cccbfffffffff55555555555555555555fffffffffffbfff
+cccbfffffffff555555ff555555fffffffffffffffffbfff
+cccbfffffffff555555fff555555ffffffffffffffffbfff
+cccbfffffffff555555fff555555ffffffffffffffffbfff
+cccbfffffffff555555ffff555555fffffffffffffffbfff
+cccbfffffffff555555fffff555555ffffffffffffffbfff
+cccbfffffffff555555fffff555555ffffffffffffffbfff
+cccbfffffffff555555ffffff555555fffffffffffffbfff
+cccbfffffffff555555fffffff555555ffffffffffffbfff
+cccbfffffffff555555fffffff555555ffffffffffffbfff
+cccbfffffffff555555ffffffff555555fffffffffffbfff
+cccbfffffffff555555fffffffff555555ffffffffffbfff
+cccbffffffff55555555ffffffff5555555fffffffffbfff
+cccbffffff555555555555fffffff55555555fffffffbfff
+cccbfffff55555555555555fffffff555555555fffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbffffffffffffffffffffffffffffffffffffffffbfff
+cccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbfff
+ccbffffffffffffffffffffffffffffffffffffffffffbff
+cbffffffffffffffffffffffffffffffffffffffffffffbf
+bffffffffffffffffffffffffffffffffffffffffffffffb
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "flare":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . 1 . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 1 1 . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . 1 1 1 1 1 1 1 1 1 1 1 . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. 1 . . . . . 1 . . . . . 1 . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. 1 . . . . . 1 . . . . . 1 . 
+. . . . . . . . . . . . . . . 
+`, img`
+1 . . . . . . 1 . . . . . . 1 
+. 1 . . . . . 1 . . . . . 1 . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. 1 . . . . . 1 . . . . . 1 . 
+1 . . . . . . 1 . . . . . . 1 
+`, img`
+. . . . . . . . . . . . . . . 
+. 1 . . . . . 1 . . . . . 1 . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. 1 . . . . . 1 . . . . . 1 . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . 1 1 1 1 1 1 1 1 1 1 1 . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . 1 . . . . 1 . . . . 1 . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . 1 . . . 1 . . . 1 . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . 1 1 1 1 1 1 1 . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . 1 . . 1 . . 1 . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . 1 . 1 . 1 . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . 1 1 1 . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . 1 . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
