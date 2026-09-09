1759bd2acb0f9445b883ec88d297a19166daf53a
Alex Kulcsar <38046796+alex-kulcsar@users.noreply.github.com>
2026-09-08 23:58:29 -0400

    Move tests out of the way.

--- test.ts
+++ test.ts
@@ -1,1 +1,51 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/**
+ * Tests
+ */
+
+/*
+Players.register(2)
+Players.register(1)
+g_gameType = {
+        name: "Quick Game",
+        rounds: "LLNC",
+        time: 10
+}
+g_currentRound = 0
+g_playerInControl = 2
+Tutorial.enable()
+beginRound()
+*/
+
+/*
+for (let i: number = 1; i < 5; i++) {
+    if (i != 3) {
+        Players.register(i)
+    }
+}
+Countdown.lettersDeclareTest()
+Countdown.letterSolveMpTest()
+g_gameType = {
+    name: "Quick Game",
+    rounds: "LLNC",
+    time: 10
+}
+g_scoreMode = {
+    name: "Competitive",
+    desc: "Whatever"
+}
+g_currentRound = 0
+g_playerInControl = 2
+Tutorial.enable()
+g_gameMode = SpriteKind.None
+Countdown.startLettersRound()
+for (let c: number = 0; c < 5; c++) {
+    Countdown.addConsonant()
+}
+for (let v: number = 0; v < 4; v++) {
+    Countdown.addVowel()
+}
+Countdown.findLetterPuzzleSolution()
+beginLettersScore()
+g_gameMode = SpriteKind.LettersBoardScore
+*/
+
